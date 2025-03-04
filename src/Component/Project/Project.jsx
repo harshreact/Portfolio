f38706ac72bd1e81eccffba788c6481fba55eb88
Harsh2703 <pharsh2703@gmail.com>
2025-03-04 14:18:05 +0530

    dark mood changes

--- src/Component/Project/Project.jsx
+++ src/Component/Project/Project.jsx
@@ -3,13 +3,15 @@ import valorant from '/src/assets/Project/Valorant.png'
 import weather from '/src/assets/Project/Weather.png'
 import Hommies from '/src/assets/Project/Hommies.jpg'
 import Grocery from '/src/assets/Project/Grocery.jpg'
+import Hand from '/src/assets/Project/Hand-Gesture.png'
+import Purchase from '/src/assets/Project/Purchase-History.png'
 import { FaGithub } from "react-icons/fa";
 
 const Project = () => {
   return (
     <section className="project" id="project">
         <div className="container">
-            <div className="main project-main">
+            <div className="main project-main">                                                                                         
                 <div className='project-top'>
                     <div className="title_h2">
                     Featured Projects
@@ -65,11 +67,34 @@ const Project = () => {
                             </div>
                         </div>
                     </div>
+                    {/*Top Project-3*/}
+                    <div className="project-block">
+                        <div className="project-image">
+                            <img src={Hand}/>
+                        </div>
+                        <div className="project-content">
+                            <div className="project-heading">
+                                <h3>Hand-Gesture Recognition Model</h3>
+                                <h5>It is a Machine Learning model, developed to identify and classify different hand gestures from videos and photos, enabaling intuitive human-computer interaction and gesture-based control systems.</h5>
+                            </div>
+                            <div className="project-links">
+                                <ul>
+                                    <li>Python</li>
+                                    <li>Machine Learning</li>    
+                                </ul>
+                                <ul className="icons">    
+                                    <li>
+                                        <a href="https://github.com/Harsh2073/Hand-Gesture.git">
+                                            <FaGithub />
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="project-bottom">
-                    <div className="title_h2">
-                        Other Projects
-                    </div>
+                    <h2>Other Projects</h2>
                     <div className='other-project'>
                         {/*Bottom Project-1*/}
                         <div className='project-bottom-block'>
@@ -116,6 +141,31 @@ const Project = () => {
                                     <ul className="icons">    
                                         <li>
                                             <a href="https://github.com/Harsh2073/Grocery.git">
+                                                <FaGithub />
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        {/*Bottom Project-3*/}
+                        <div className='project-bottom-block'>
+                            <div className="bottom-block-image">
+                                <img src={Purchase}/>
+                            </div>
+                            <div className="bottom-block-content">
+                                <div className="bottom-block-heading">
+                                    <h3>Purchase History</h3>
+                                    <h5>Its a Machine Learning model, developed to group or to make a cluster of customers based of their purchasing history, K-Mean Clusting algorithm is used to dvelop this model.</h5>
+                                </div>
+                                <div className="bottom-block-links">
+                                    <ul>
+                                        <li>Python</li>
+                                        <li>Machine Learning</li>
+                                    </ul>
+                                    <ul className="icons">    
+                                        <li>
+                                            <a href="https://github.com/Harsh2073/Purchase-History.git">
                                                 <FaGithub />
                                             </a>
                                         </li>
